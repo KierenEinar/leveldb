7b14a1ed31d45cc4359f1946c7ef026be6613f00
jianghongbin <JIANGHONGBIN@kingsoft.com>
2023-02-21 20:15:45 +0800

    add version builder test

--- version_set_test.go
+++ version_set_test.go
@@ -2,6 +2,7 @@ package leveldb
 
 import (
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -12,101 +13,115 @@ func Test_VersionBuilder(t *testing.T) {
 	t.Run("test add and delete table file", func(t *testing.T) {
 
 		vSet := newVersionSet(opt)
+		fatalInitVersion(t, vSet)
 
-		vb := newBuilder(vSet, nil)
-
-		// add level'0 new table
+		// del level 1 [1E, 1F], [1G, 1H], [1Q, 1R]
+		// add level 1 [1EA, 1FA], [1GA, 1GH], [1GI, 1GZ], [1QA, 1QZ]
 		edit := VersionEdit{
+			delTables: []delTable{
+				{
+					level:  1,
+					number: 12,
+				},
+				{
+					level:  1,
+					number: 13,
+				},
+				{
+					level:  1,
+					number: 18,
+				},
+			},
 			addedTables: []addTable{
 				{
-					level:  0,
-					size:   10,
-					number: 9,
-					imin:   buildInternalKey(nil, []byte("abyu"), keyTypeValue, 100),
-					imax:   buildInternalKey(nil, []byte("zxya"), keyTypeValue, 100),
+					level:  1,
+					number: 1001,
+					imin:   buildInternalKey(nil, []byte("1EA"), keyTypeValue, 100),
+					imax:   buildInternalKey(nil, []byte("1FA"), keyTypeValue, 100),
+				},
+				{
+					level:  1,
+					number: 1002,
+					imin:   buildInternalKey(nil, []byte("1GA"), keyTypeValue, 100),
+					imax:   buildInternalKey(nil, []byte("1GH"), keyTypeValue, 100),
+				},
+				{
+					level:  1,
+					number: 1003,
+					imin:   buildInternalKey(nil, []byte("1GI"), keyTypeValue, 100),
+					imax:   buildInternalKey(nil, []byte("1GZ"), keyTypeValue, 100),
+				},
+				{
+					level:  1,
+					number: 1004,
+					imin:   buildInternalKey(nil, []byte("1QA"), keyTypeValue, 100),
+					imax:   buildInternalKey(nil, []byte("1QZ"), keyTypeValue, 100),
 				},
 			},
 		}
-
 		v := &Version{}
-
+		vb := newBuilder(vSet, vSet.current)
 		vb.apply(edit)
 		vb.saveTo(v)
-
-		if v.levels[0][0].fd != int(edit.addedTables[0].number) {
-			t.Fatalf("level[0][0] should eq ")
+		vSet.current = v
+		for i := 0; i < len(vSet.current.levels[1]); i++ {
+			tfile := vSet.current.levels[1][i]
+			t.Logf("tFile==>fd[%d], imin[%s], imax[%s]", tfile.fd, tfile.iMin.userKey(), tfile.iMax.userKey())
 		}
-
-		// add level'0 new table
-		edit = VersionEdit{
-			addedTables: []addTable{
-				{
-					level:  0,
-					size:   10,
-					number: 10,
-					imin:   buildInternalKey(nil, []byte("oews"), keyTypeValue, 100),
-					imax:   buildInternalKey(nil, []byte("yw"), keyTypeValue, 100),
-				},
-			},
-		}
-
-		v2 := &Version{}
-
-		vb = newBuilder(vSet, v)
-		vb.apply(edit)
-		vb.saveTo(v2)
-
-		if v2.levels[0][1].fd != int(edit.addedTables[0].number) {
-			t.Fatalf("level[0][1] should eq ")
-		}
-
-		// add level'0 new table
-
-		edit = VersionEdit{
-			addedTables: []addTable{
-				{
-					level:  0,
-					size:   10,
-					number: 12,
-					imin:   buildInternalKey(nil, []byte("rews"), keyTypeValue, 100),
-					imax:   buildInternalKey(nil, []byte("uw"), keyTypeValue, 100),
-				},
-			},
-		}
-
-		v3 := &Version{}
-
-		vb = newBuilder(vSet, v2)
-		vb.apply(edit)
-		vb.saveTo(v3)
-
-		if v3.levels[0][2].fd != int(edit.addedTables[0].number) {
-			t.Fatalf("level[0][2] should eq ")
-		}
-
-		// add level'0 new table
-		edit = VersionEdit{
-			addedTables: []addTable{
-				{
-					level:  0,
-					size:   10,
-					number: 12,
-					imin:   buildInternalKey(nil, []byte("xews"), keyTypeValue, 100),
-					imax:   buildInternalKey(nil, []byte("zw"), keyTypeValue, 100),
-				},
-			},
-		}
-
-		v4 := &Version{}
-
-		vb = newBuilder(vSet, v3)
-		vb.apply(edit)
-		vb.saveTo(v4)
-
-		if v4.levels[0][3].fd != int(edit.addedTables[0].number) {
-			t.Fatalf("level[0][2] should eq ")
-		}
-
 	})
+
+}
+
+func fatalInitVersion(t *testing.T, vSet *VersionSet) {
+
+	// each level design 10 sstable file
+	var lastVer *Version
+	for i := 0; i < KLevelNum; i++ {
+		c := rune(65)
+		step := 1
+
+		for j := 0; j < 10; j++ {
+			if i == 0 {
+				step = 8
+			}
+			si := c
+			ei := c + rune(step)
+			s := strconv.Itoa(i) + string(si)
+			e := strconv.Itoa(i) + string(ei)
+			imin := buildInternalKey(nil, []byte(s), keyTypeValue, 100)
+			imax := buildInternalKey(nil, []byte(e), keyTypeValue, 100)
+
+			if i == 0 {
+				c = (si + ei) / 2
+			} else {
+				c = ei + 1
+			}
+
+			t.Logf("level i=%d, min=%s, max=%s, number=%d", i, s, e, i*10+j)
+
+			edit := VersionEdit{
+				addedTables: []addTable{
+					{
+						level:  i,
+						size:   10,
+						number: uint64(i*10 + j),
+						imin:   imin,
+						imax:   imax,
+					},
+				},
+			}
+
+			v := &Version{}
+			vb := newBuilder(vSet, lastVer)
+			vb.apply(edit)
+			vb.saveTo(v)
+			lastVer = v
+			vSet.current = v
+			if v.levels[i][j].fd != int(edit.addedTables[0].number) {
+				t.Fatalf("level[i][j] should eq ")
+			}
+
+		}
+	}
 
 }
